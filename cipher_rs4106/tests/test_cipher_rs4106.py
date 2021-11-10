d970f16febd68284d203e252311ddef40a3839ea
rspanierman <90914119+rspanierman@users.noreply.github.com>
2021-11-10 17:12:21 -0500

    test

--- cipher_rs4106/tests/test_cipher_rs4106.py
+++ cipher_rs4106/tests/test_cipher_rs4106.py
@@ -1,1 +1,11 @@
 from cipher_rs4106 import cipher_rs4106
+
+
+def test_cipher():
+    example1 = 'alphabet'
+    example2 = -2
+    expected = 'YjnfYZcr'
+    actual = cipher(example1, example2)
+    assert actual == expected
+
+test_cipher()
