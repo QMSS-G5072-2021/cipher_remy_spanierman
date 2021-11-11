160ae1b58873ce655aafdb56da7a91623c4941d8
rspanierman <90914119+rspanierman@users.noreply.github.com>
2021-11-10 20:50:59 -0500

    sphinx

--- cipher_rs4106/tests/test_cipher_rs4106.py
+++ cipher_rs4106/tests/test_cipher_rs4106.py
@@ -1,11 +1,10 @@
-Import py.test
 from cipher_rs4106 import cipher_rs4106
 
 def test_cipher():
     example1 = 'alphabet'
     example2 = -2
     expected = 'YjnfYZcr'
-    actual = cipher(example1, example2)
+    actual = cipher_rs4106.cipher(example1, example2)
     assert actual == expected
 
 test_cipher()
